17e348491d5d96b0e8f3a1f06a26db4ee4638846
Dear Bear <dearerbear@gmail.com>
2018-08-02 19:33:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   row_input = ["   ", "|", " x  ", "|", "   "]
-#  test_row = row_input.each { |a| print a }
+  test_row = row_input.each { |a| print a }
   row = String.new(row_input[0] + row_input[1] + row_input[2] + row_input[3] + row_input[4])
-  row = String.new("#{test_row}")
   line = "-----------"
   puts row
   puts line
   puts row
   puts line
   puts row
-  #puts test_row
+  puts test_row
 end
 
 display_board
