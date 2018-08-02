0f45ebb62656719dcd547471fa8012f5ccce46da
Dear Bear <dearerbear@gmail.com>
2018-08-02 19:10:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  row = String.new()["   ", "|", "   ", "|", "   "])
+  row = String.new(["   ", "|", "   ", "|", "   "])
   line = "-----------"
   puts row
   puts line
